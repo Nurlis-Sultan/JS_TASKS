1f7b4d07ec474c6f6a18e2d1982db5a28be9bcb7
Nurlis-Sultan <116050561+Nurlis-Sultan@users.noreply.github.com>
2022-10-25 10:47:26 +0600

    fead:add new task

---
+++ 25.10/script.js
@@ -0,0 +1,17 @@
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+let hour = 10;
+let min = 30;
+let sec = 55;
+let time = `${hour}:${min}:${sec}`;
+console.log(time);
+
+var num = 10;
+num = num++;
+// num = ++num;
+alert(num);
