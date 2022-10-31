376ca06358161a2bd12ee775b4ecd99615e6052f
Nurlis-Sultan <116050561+Nurlis-Sultan@users.noreply.github.com>
2022-10-31 12:54:50 +0600

    init: task31.10

---
+++ 31.10/script.js
@@ -0,0 +1,45 @@
+/*
+1) Создайте объект заработных плат.
+    Назовите userSalaries.
+    Свойствами выступают: Ella, Sophia, Ellie
+Выведите на экран зарплату Ella, Sophia.
+
+2) Создайте объект с ключами a, b и c значениями 5 , 6 и 10.
+Найдите сумму его элементов.
+
+
+3) let number;
+if(data === true){
+    number = 3;
+} else{
+    number = 5;
+}
+
+Сделайте рефакторинг кода, с помощью тернарного оператора.
+*/
+
+/********** 1) **********/
+// let userSalaries = {
+//     Ella: 15,
+//     Sophia: 20,
+//     Ellie: 25
+// }
+// console.log(`zarplata  Ella:${userSalaries.Ella}, zarplata  Sophia: ${userSalaries.Sophia}`);
+
+/********** 2) **********/
+
+// let key = {
+//     a: 5,
+//     b: 6,
+//     c: 10
+// }
+// console.log(key.a+key.b+key.c);
+
+/********** 3) **********/
+
+let number;
+let data;
+number = data ? 3 : 5;
+// number = 1 ? 3 : 5;
+alert(number);
+
